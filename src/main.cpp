65e50ff20803bf1fe53db2c6b370b1b43270d136
Miso <zakwrn@gmail.com>
2022-08-29 19:31:06 +0200

    error handling added

--- src/main.cpp
+++ src/main.cpp
@@ -6,96 +6,109 @@
 
 using namespace std;
 
-void parseIntArgs(int argNum, int i, char *argv[])
+void badArg()
 {
-    int tArray[argNum];
+    cout << "error: user provided bad arguement";
+    exit(1);
+}
+bool parseIntArgs(int argNum, int i, char *argv[], int *resArray)
+{
+    // int resArray[argNum];
     bool badConversion = false;
     for (int j = 1; j < argNum + 1; j++)
     {
         if (strcmp(to_string(atoi(argv[i + j])).c_str(), argv[i + j]) == 0)
         {
-            tArray[j - 1] = atoi(argv[i + j]);
-            // cout << j-1 << " a\n";
+            resArray[j - 1] = atoi(argv[i + j]);
         }
         else
         {
-            cout << "b\n";
             badConversion = true;
         }
     }
-    if (badConversion)
-    {
-        cout << "c\n";
-        cout << "bad conversion\n";
-    }
     if (!badConversion)
     {
-        for (int j = 0; j < argNum ; j++)
+        for (int j = 0; j < argNum; j++)
         {
-            cout << tArray[j] << "\n";
-        }
+            return true;
         }
     }
-
-    int main(int argc, char *argv[])
+    return false;
+}
+int main(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i++)
     {
-        // cout << "hello: " << argv[argc-1];
-        for (int i = 1; i < argc; i++)
+        // cout << argv[i];
+        if (strcmp(argv[i], "-t") == 0)
         {
-            if (strcmp(argv[i], "-t") == 0)
+            int tResultArr[4];
+            if (parseIntArgs(4, i, argv, tResultArr))
             {
-                parseIntArgs(4, i, argv);
+                i += 4;
             }
-            if (strcmp(argv[i], "-s") == 0)
+            else
             {
-                cout << argv[i] << " is found\n";
-                parseIntArgs(2, i, argv);
-            }
-            if (strcmp(argv[i], "-w") == 0)
-            {
-                cout << argv[i] << " is found\n";
+                badArg();
             }
         }
+        if (strcmp(argv[i], "-s") == 0)
+        {
+            int sResultArr[2];
+            if (parseIntArgs(2, i, argv, sResultArr))
+            {
+                i += 2;
+            }
+            else
+            {
+                badArg();
+            }
+        }
+        if (strcmp(argv[i], "-w") == 0)
+        {
+            cout << argv[i] << " is found\n";
+        }
     }
+}
 
-    // Remember to do error checking when parsing
-    //     your command line
+// Remember to do error checking when parsing
+//     your command line
 
-    // CLI program called buffer
-    // params are
+// CLI program called buffer
+// params are
 
-    // default constructor too:
-    // sets up deconstructor (empty arg list, not void), frees up resources when obj out of scope.
-    // FrameSequence::~FrameSequence();
-    // raw mem allocation for each frame.
-    // iterates through vec and free up allocated mem. (activated automatically)
+// default constructor too:
+// sets up deconstructor (empty arg list, not void), frees up resources when obj out of scope.
+// FrameSequence::~FrameSequence();
+// raw mem allocation for each frame.
+// iterates through vec and free up allocated mem. (activated automatically)
 
-    // output has appropriate file numbering
+// output has appropriate file numbering
 
-    // cli
-    // extractor <inputPGMfile> [options]
-    //-t <int> <int> <int> <int> # x1 y1 x2 y2 (begin & end origin pixel coords,
-    // for frame trajectory)
-    // -s <int> <int> # <width> <height> (size of small frame in
-    // pixels)
-    // -w <string> <string> # write frames with <operation> <name>
+// cli
+// extractor <inputPGMfile> [options]
+//-t <int> <int> <int> <int> # x1 y1 x2 y2 (begin & end origin pixel coords,
+// for frame trajectory)
+// -s <int> <int> # <width> <height> (size of small frame in
+// pixels)
+// -w <string> <string> # write frames with <operation> <name>
 
-    // For the - w flag, the parameter<operation> = none | invert | reverse | revinvert,
-    // where none : no modification of data, invert : each pixel value v become 255 - v,
-    // reverse : 2 reverse output(write frames out from last to first),
-    //  revinvert : reverse and invert out - put.
-    // The parameter<name> = name of sequence base e.g.sequence - 0000.pgm, sequence - 0001.pgm etc
-    // will be generated with<name> set to ‘sequence’.
-    // NOTE : -t and -s are specified once, but you can have one or more - w operations specifed,
-    // each of which outputs a different frame sequence.
+// For the - w flag, the parameter<operation> = none | invert | reverse | revinvert,
+// where none : no modification of data, invert : each pixel value v become 255 - v,
+// reverse : 2 reverse output(write frames out from last to first),
+//  revinvert : reverse and invert out - put.
+// The parameter<name> = name of sequence base e.g.sequence - 0000.pgm, sequence - 0001.pgm etc
+// will be generated with<name> set to ‘sequence’.
+// NOTE : -t and -s are specified once, but you can have one or more - w operations specifed,
+// each of which outputs a different frame sequence.
 
-    // ./extractor myLargeImage.pgm - t 0 10 5000 5000 - s 640 480 - w invert invseq - w none sequence2
-    // You must format the output string for the frame file name to have enough leading 0’s
-    // to ensure you can hold all your frames (up to 4 digits should be fine e.g. someting like
-    // “%04d”). There are string format commands to manage this with C++ streams and
-    // stringstreams.
+// ./extractor myLargeImage.pgm - t 0 10 5000 5000 - s 640 480 - w invert invseq - w none sequence2
+// You must format the output string for the frame file name to have enough leading 0’s
+// to ensure you can hold all your frames (up to 4 digits should be fine e.g. someting like
+// “%04d”). There are string format commands to manage this with C++ streams and
+// stringstreams.
 
-    // provide a README file explaining what each file submitted does and
-    //     how it fits into the program as a whole.The README file should not explain any
-    //         theory that you have used.The README be used by the tutors if they encounter
-    //             any problems
+// provide a README file explaining what each file submitted does and
+//     how it fits into the program as a whole.The README file should not explain any
+//         theory that you have used.The README be used by the tutors if they encounter
+//             any problems
